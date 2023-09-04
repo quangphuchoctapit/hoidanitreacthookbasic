1a0812b1609c5868423f318e9baa38f909265c64
Fuc le <quangphuchoctapit@gmail.com>
2023-09-04 11:46:55 +0700

    multiple components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/Nav'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Nav />
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hell odog </h1>
       </header>

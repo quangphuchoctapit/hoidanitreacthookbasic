7694e602bdd4eddb0f4dbb921fb04bd85384f579
Fuc le <quangphuchoctapit@gmail.com>
2023-09-04 12:51:00 +0700

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav'
+import NumberCounter from './views/NumberCounter'
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const handleDecrease = () => {
+    setCount(count - 1)
+  }
+  const handleIncrease = () => {
+    setCount(count + 1)
+
+  }
+  const handleReset = () => {
+    setCount(0)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <Nav />
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hell odog </h1>
+        <NumberCounter decrease={handleDecrease} increase={handleIncrease} reset={handleReset} count={count} />
       </header>
     </div>
   );

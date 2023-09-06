e7a0664a2c7f5d788fd19f6de43db4d9fea353bd
Fuc le <quangphuchoctapit@gmail.com>
2023-09-06 12:31:30 +0700

    backdata with useHistory

--- src/views/DetailBlog.jsx
+++ src/views/DetailBlog.jsx
@@ -1,12 +1,17 @@
-import { useParams } from "react-router-dom"
+import { useParams, useHistory } from "react-router-dom"
 
 const DetailBlog = () => {
     let { id } = useParams()
+    let backData = useHistory()
     console.log('check useParam: ', id)
+    const handleBackData = () => {
+        backData.push('/blog')
+    }
     return (
-        <div>
-            Hello blog with id={id}
-        </div>
+        <>
+            <div><span style={{ backgroundColor: '#ccc', padding: '5px 10px', borderRadius: '5px', color: 'black', cursor: 'pointer' }} onClick={handleBackData}>&lt;-- Back</span></div>
+            <div>Hello blog with id={id}</div>
+        </>
     )
 }
 
